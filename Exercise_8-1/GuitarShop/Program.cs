01ffd1f485ece6328e2616de2f0d4d78668a6b8f
Dean-5464 <87671890+Dean-5464@users.noreply.github.com>
2021-07-22 11:53:34 -0500

    Add files via upload

---
+++ Exercise_8-1/GuitarShop/Program.cs
@@ -0,0 +1,21 @@
+﻿//Dean Kammerer
+//3/7/2021 - 3/8/2021
+//Exercise 8-1
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace GuitarShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
