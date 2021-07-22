d9df2a9857b5b27f6b90d0e7d51ca68a0954e697
Dean-5464 <87671890+Dean-5464@users.noreply.github.com>
2021-07-22 12:02:10 -0500

    Add files via upload

---
+++ Exercise_4-1/MovieList/Program.cs
@@ -0,0 +1,24 @@
+//Dean Kammerer
+//2/15/2021 - 3/2/2021
+//Exercise 4-1
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace MovieList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
