39f8c1290a3033bf91d777a236c936af3135288d
Dean-5464 <87671890+Dean-5464@users.noreply.github.com>
2021-07-22 12:04:22 -0500

    Add files via upload

---
+++ Project_9/Project_9/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Project_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.DisplayGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(179, 305);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(99, 30);
+            this.LoadButton.TabIndex = 0;
+            this.LoadButton.Text = "Load Data";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(487, 305);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(112, 30);
+            this.UpdateButton.TabIndex = 1;
+            this.UpdateButton.Text = "Update Data";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // DisplayGrid
+            // 
+            this.DisplayGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DisplayGrid.Location = new System.Drawing.Point(77, 50);
+            this.DisplayGrid.Name = "DisplayGrid";
+            this.DisplayGrid.RowHeadersWidth = 51;
+            this.DisplayGrid.RowTemplate.Height = 24;
+            this.DisplayGrid.Size = new System.Drawing.Size(626, 226);
+            this.DisplayGrid.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DisplayGrid);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.LoadButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DisplayGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.DataGridView DisplayGrid;
+    }
+}
+
---
+++ Project_9/Project_9/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Project_9
+{
+    public partial class Form1 : Form
+    {
+
+        private OleDbConnection dbConn;
+        private OleDbCommand dbCmd;
+        private string SConnection;
+        private string sql;
+
+        private OleDbDataAdapter Adapt;
+        private DataSet CustomerDs;
+        private OleDbCommandBuilder CmdBuilder;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            //Method Below
+            LoadData();
+
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CmdBuilder = new OleDbCommandBuilder(Adapt);
+                Adapt.Update(CustomerDs, "CustomerTable");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Data Update Error");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Method Below
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            try
+            {
+
+                SConnection = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source = Customers.accdb";
+
+                dbConn = new OleDbConnection(SConnection);
+                dbConn.Open();
+
+                sql = "Select * From CustomerTable";
+
+                dbCmd = new OleDbCommand();
+                dbCmd.CommandText = sql;
+
+                dbCmd.Connection = dbConn;
+
+                Adapt = new OleDbDataAdapter();
+                Adapt.SelectCommand = dbCmd;
+                CustomerDs = new DataSet();
+                Adapt.Fill(CustomerDs, "CustomerTable");
+                DisplayGrid.DataSource = CustomerDs;
+                DisplayGrid.DataMember = "CustomerTable";
+
+
+                dbConn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Occurred");
+            }
+        }
+    }
+}
